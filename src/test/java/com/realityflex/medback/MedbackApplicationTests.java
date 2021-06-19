ebc014ced12962e1b501b1cd51cb10798b53bfb1
ginfin <gara_0100@mail.ru>
2021-06-19 06:33:18 +0300

    add build.gradle

---
+++ src/test/java/com/realityflex/medback/MedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realityflex.medback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
