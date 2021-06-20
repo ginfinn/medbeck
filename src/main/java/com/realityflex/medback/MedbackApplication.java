013adaea855246e2f412ab1b89b9eafbb58e2e6b
ginfin <gara_0100@mail.ru>
2021-06-20 05:34:17 +0300

    add auto filling role repository

--- src/main/java/com/realityflex/medback/MedbackApplication.java
+++ src/main/java/com/realityflex/medback/MedbackApplication.java
@@ -1,7 +1,11 @@
 package com.realityflex.medback;
 
+import com.realityflex.medback.entity.Role;
+import com.realityflex.medback.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MedbackApplication {
@@ -11,5 +15,12 @@ public class MedbackApplication {
     }
 
 
+    @Bean
+    public CommandLineRunner loadData(RoleRepository repository) {
+        return (args) -> {
+            repository.save(new Role("1", "ROLE_DOCTOR"));
+            repository.save(new Role("2", "ROLE_PATIENT"));
 
+        };
+    }
 }
