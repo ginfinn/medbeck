f24f01053a63b38df95425f624de6e023e56277a
Alexander Lev <Sashalev200149@gmail.com>
2021-06-20 03:05:53 +0300

    Add two windows and something also

--- src/main/java/com/realityflex/medback/controller/MainController.java
+++ src/main/java/com/realityflex/medback/controller/MainController.java
@@ -30,6 +30,7 @@ public class MainController {
     @Autowired
     PressureRepository pressureRepository;
 
+
     //    @Autowired
 //    PhotoService photoService;
     @PostMapping("/register/patient")
